aa0dfbb38d81d2048e635f2746b5b4a2589fc353
rvirdiz <ramanvirdiz@gmail.com>
2015-04-13 11:51:06 +0530

    Added end_time, BookingForm changed

--- src/home/forms.py
+++ src/home/forms.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-# from django.contrib.admin import widgets
+from django.db.models import Q
 from django import forms
 from .models import Booking
 from datetime import timedelta
@@ -9,33 +9,38 @@ class BookingForm(forms.ModelForm):
 	# This connects the 'Booking' model with this form and shows the specified fields on the html.
 	class Meta:
 		model = Booking 
-		fields = ["hall", "date", "duration", "start_time", "event_name", "name", "email"]
+		exclude = ['status', 'email']
 
-	# With the help of the AdminDateWidget, a calender for the date input can be used.
-	# date = forms.DateField(widget=widgets.AdminDateWidget)
+
 	"""
-	If the input date is not valid then it will show a validation message
+	If the input date is not valid then it will show a validation message.
+	The form will also raise a validation error if the start time or the end time of the event is between the timings for 
+	already booked or hall to be booked. e.g. If the hall is booked from 8AM to 10AM, then the user can't book the hall 
+	from 9AM to 10AM.
 	"""
-	def clean_date(self):
-		date = self.cleaned_data['date']
+	def clean(self):
+		form_data = self.cleaned_data
+		event_date = form_data['date']
 
-		if date < datetime.date.today():
-			raise forms.ValidationError("Must be a future date")
-		return date
+		if event_date < datetime.date.today():
+			raise forms.ValidationError("Invalid Date")
 
-	def clean_time(self):
-		data = self.cleaned_data
-		event_start = cleaned_data.get('start_time')
-		event_hours = cleaned_data.get('duration')
-		event_end = ('event_start' + timedelta(hours = 'event_hours')).strftime('%H:%M %p')
-		event_hall = cleaned_data.get('hall')
-		event_date = cleaned_data.get('date')
-		end_time = ('start_time' + timedelta(hours = 'duration')).strftime('%H:%M %p')
-		event_time = Booking.objects.filter(hall = event_hall, date = 'event_date', \
-			start_time__gte = 'event_start', end_time__lte = 'event_end').values('start_time')
+		event_start = form_data['start_time']
+		event_end = form_data['end_time']
+		# FMP = '%H:%M %p'
+		# duration = datetime.strptime(event_end, FMP) - datetime.strptime(event_start, FMP)
+		# event_end = (event_start + timedelta(hours = event_hours)).strftime('%H:%M %p')
+
+		if event_start >= event_end:
+			raise forms.ValidationError("Timings should be valid")
+
+		event_hall = form_data['hall']
+		event_time = Booking.objects.filter(Q(start_time__gte = event_start, end_time__lte = event_end) | \
+			Q(start_time__lte = event_start, end_time__gte = event_end), \
+			date = event_date, hall = event_hall)
 
 		for t in event_time:
 			if t is not None:
-				raise forms.ValidationError("Invalid time")
+				raise forms.ValidationError("Invalid Time")
 
-		return data
+		return form_data
