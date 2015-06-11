5d1c26477ba557f99a047a55245df24cc6a584b2
rvirdiz <ramanvirdiz@gmail.com>
2015-06-11 12:12:58 +0530

    Fixed bugs in forms.py

--- src/home/forms.py
+++ src/home/forms.py
@@ -23,49 +23,63 @@ class BookingForm(forms.ModelForm):
 	the hall from 9AM to 10AM.
 	"""
 	def clean(self):
-		try:	
-			form_data = self.cleaned_data
-			event_date = form_data['date']
-			event_hall = form_data['hall']
-			event_start = form_data['start_time']
-			event_end = form_data['end_time']
+	# try:	
+		# Getting form data used for checking whether the booking can be done or not for filled entries.
+		form_data = self.cleaned_data
+		event_date = form_data['date']
+		event_hall = form_data['hall']
+		event_start = form_data['start_time']
+		event_end = form_data['end_time']
+		
+		# Getting the current date and time for comparing
+		time_now = datetime.datetime.now().strftime('%H:%M:00')
+		date_today = datetime.date.today()
 
-			# Getting the current date and time for comparing
-			time_now = datetime.datetime.now().strftime('%H:%M:00')
-			date_today = datetime.date.today()
+		# For converting event_start from datetime.time to datetime.datetime, combine method is used for it and then 
+		# adding or subtracting number of hours from it.
+		e_start = (datetime.datetime.combine(datetime.date.today(), event_start) + datetime.timedelta(minutes = 1)).\
+		strftime('%H:%M:00')
+		e_end = (datetime.datetime.combine(datetime.date.today(), event_end) - datetime.timedelta(minutes = 1)).\
+		strftime('%H:%M:00')
 
-			# FMP = '%H:%M %p'
-			# duration = datetime.datetime.strptime(event_end, FMP) - datetime.datetime.strptime(event_start, FMP)
-			# event_end = (event_start + timedelta(hours = event_hours)).strftime('%H:%M %p')
+		# For checking if the start_time is less than end_time such that timing should be valid.
+		if (event_start >= event_end):
+			raise forms.ValidationError("Invalid Timings")
 
-			
-			# For checking if the start_time is less than end_time, valid timings
-			if (event_start >= event_end):
-				raise forms.ValidationError("Invalid Timings")
+		# For checking duration of the event, as it should not be more than 5 hours.
+		duration = ((datetime.datetime.combine(datetime.date.today(), event_end) - datetime.datetime.combine(datetime.date.\
+			today(), event_start))/60).seconds
+		if (duration % 60 != 0 and (duration / 60) == 5):
+			raise forms.ValidationError("Duration should be less than 5 hours")
+		elif ((duration / 60) > 5):
+			raise forms.ValidationError("Duration should be less than 5 hours")
+		else:
+			print ("Valid Duration")
 
-			# For checking event booking date and timings such that no past booking can be done such that for today's 
-			# date and the start_time as well as end_time should be greater than current time whereas the isoformat is 
-			# used for converting the time to the string and comparing the value with the form time values
-			if (event_date == date_today and (event_start.isoformat() <= time_now or event_end.isoformat() <= \
-				time_now)):
-				raise forms.ValidationError("Please fill future timings")
+		# For checking event booking date and timings such that no past booking can be done such that for today's 
+		# date and the start_time as well as end_time should be greater than current time whereas the isoformat is 
+		# used for converting the time to the string and comparing the value with the form time values
+		if (event_date == date_today and (event_start.isoformat() <= time_now or event_end.isoformat() <= \
+			time_now)):
+			raise forms.ValidationError("Please fill future timings")
 
-			# Comparing the form_data with the values in database for checking already booked hall
-			event_time = Booking.objects.filter(Q(start_time__range = (event_start, event_end)) | \
-				Q(end_time__range = (event_start, event_end)) | \
-				Q(start_time__lte = event_start, end_time__gte = event_end) | \
-				Q(start_time__gte = event_start, end_time__lte = event_end), \
-				date = event_date, hall = event_hall)
+		# Comparing the form_data with the values in database for checking already booked hall
+		event_time = Booking.objects.filter(Q(start_time__range = (e_start, e_end)) | \
+			Q(end_time__range = (e_start, e_end)) | \
+			Q(start_time__lte = event_start, end_time__gte = event_end) | \
+			Q(start_time__gte = event_start, end_time__lte = event_end), \
+			date = event_date, hall = event_hall)
 
-			for t in event_time:
-				if t is not None:
-					raise forms.ValidationError("Hall Already Booked")
+		# If any any booking exists within the 
+		for t in event_time:
+			if t is not None:
+				raise forms.ValidationError("Hall Already Booked")
 
-			return form_data
+		return form_data
 
-		except KeyError:
-			print ("Please fill the entries")
-		# If the exception is raised for KeyError, then the statement will execute till the required entries are filled
+	# except KeyError:
+		# print ("Please fill the entries")
+	# If the exception is raised for KeyError, then the statement will execute till the required entries are filled
 
 
 """
